df5354a7f7a703c91fd1cc2f4d0e2e972a2f533e
Hanz <hanznikkolmaas@gmail.com>
2025-12-11 11:43:13 +0800

    Archive List Fetching fix

--- src/app/layout/Header/HeaderItems/History/ArchiveList.tsx
+++ src/app/layout/Header/HeaderItems/History/ArchiveList.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogDescription, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { MergedHistoryItem } from '@/lib/history/historyTypes';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import HistoryItemComponent from './HistoryItemComponent';
-import { DialogDescription } from '@radix-ui/react-dialog';
+import LoadingSpinner from '@/app/main_components/LoadingSpinner';
 
 interface ArchiveListProps {
   open: boolean,
@@ -13,9 +13,10 @@ interface ArchiveListProps {
   onDeletePermanently: (item: MergedHistoryItem) => void
   onClickItem: (item: MergedHistoryItem) => void,
   loadingItemId?: string | null
+  loading?: boolean
 }
 
-function ArchiveList({open, onOpenChange, archivedItems, onRestore, onDeletePermanently, onClickItem, loadingItemId}: ArchiveListProps) {
+function ArchiveList({open, onOpenChange, archivedItems, onRestore, onDeletePermanently, onClickItem, loadingItemId, loading}: ArchiveListProps) {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
         <DialogContent className='max-h-[80vh] max-w-lg p-4 flex flex-col'>
@@ -25,7 +26,9 @@ function ArchiveList({open, onOpenChange, archivedItems, onRestore, onDeletePerm
             </DialogHeader>
 
             <ScrollArea className="mt-2 flex-1 overflow-y-auto">
-                {archivedItems.length === 0 ? (
+                {loading ? (
+                    <LoadingSpinner/>
+                ): archivedItems.length === 0 ? (
                     <p className="text-center text-gray-400">No archived items.</p>
                 ) : (
                     <ul className='space-y-3'>
